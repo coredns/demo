b9db840b7a152fd051194cb11779f2808cd077cc
Yong Tang <yong.tang.github@outlook.com>
2023-04-20 19:16:58 +0000

    Fix init change
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- setup.go
+++ setup.go
@@ -6,7 +6,12 @@ import (
 	"github.com/coredns/coredns/plugin"
 )
 
-func init() { plugin.RegisterPlugin("demo", setup) }
+func init() {
+	caddy.RegisterPlugin("demo", caddy.Plugin{
+		ServerType: "dns",
+		Action:     setup,
+	})
+}
 
 func setup(c *caddy.Controller) error {
 	c.Next() // 'demo'
